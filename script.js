445836c2c6c093acbd6304ef394baf2aed629a92
NICOLAS DE OLIVEIRA PACHECO <nicolas_o_pacheco@estudante.sesisenai.org.br>
2024-10-29 19:46:41 -0300

    biblioteca sendo criada

---
+++ script.js
@@ -0,0 +1,9 @@
+let bancoDeDados = [];
+let inputTitulo = document.getElementById("cadTitulo");
+let inputAutor = document.getElementById("cadAutor");
+let inputGenero = document.getElementById("cadGen");
+let inputEditora = document.getElementById("cadEditora");
+
+function add(){
+    
+}
