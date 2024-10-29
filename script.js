9229dbe7b3e1bdbff26cb23d1f8c64609cf26f24
Nicolas de Oliveira Pacheco <104364831+NicolasOliveiraP@users.noreply.github.com>
2024-10-29 19:49:40 -0300

    biblioteca
    Criação da biblioteca aula SESISENAI

---
+++ script.js
@@ -0,0 +1,12 @@
+let bancoDeDados = [];
+let inputTitulo = document.getElementById("cadTitulo");
+let inputAutor = document.getElementById("cadAutor");
+let inputGenero = document.getElementById("cadGen");
+let inputEditora = document.getElementById("cadEditora");
+
+function add(){
+    let livro = {
+        titulo:inputTitulo.value,
+        
+    }
+}
